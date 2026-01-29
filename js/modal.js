556a1e9f8311bfedae520315f1d5b46a974d5504
Nataliia Balashova <88karamelka88@mail.ru>
2026-01-29 12:59:17 +0300

    added modal

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+const modalButton = document.querySelectorAll('.more')
+const modal = document.querySelector('.modal')
+const overlay = modal.querySelector('.overlay')
+const closeButton = modal.querySelector('.modal__close')
+let isOpen = true
+
+
+if (isOpen){
+    modalButton.forEach((btn) =>{
+        btn.addEventListener('click', ()=>{
+            modal.classList.remove('hidden')
+        })
+    })
+    
+}
+
+// modalButton.addEventListener('click', ()=>{
+//     modal.classList.remove('hidden')
+// })
+
+overlay.addEventListener('click', ()=>{
+    modal.classList.add('hidden')
+})
+
+closeButton.addEventListener("click", ()=>{
+    modal.classList.add('hidden')
+})
